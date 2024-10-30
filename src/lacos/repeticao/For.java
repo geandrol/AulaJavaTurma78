db55ca3ab2bab2060a98b29ffff9f2e8074475b9
geandro <geandro_a@hotmail.com>
2024-10-30 11:31:04 -0300

    Aula lços reperição

---
+++ src/lacos/repeticao/For.java
@@ -0,0 +1,24 @@
+package lacos.repeticao;
+
+import java.util.Scanner;
+
+public class For {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);	
+        
+		System.out.print("Digite um número para ver sua tabuada: ");
+		int numero = scanner.nextInt();
+		
+		
+		for(int cont = 0; cont <= 10; cont++) {
+			int resul = numero * cont;
+			System.out.println(numero + " x " + cont + " = " + resul);
+			
+			System.out.println("Segunda forma:"+numero * cont);
+		}
+		
+	}
+
+}
